06df26ac1877ba2f46b16201572f29d0252ae059
Joep van Dijk <joep.van.dijk04@gmail.com>
2020-12-05 16:26:20 +0100

    Bug fixed..?

--- cmd/Dashboard/static/js/index.js
+++ cmd/Dashboard/static/js/index.js
@@ -1,5 +1,6 @@
 let lastID = 0;
 let count = 0;
+let length = 0;
 
 (async () => {
     let chart = document.createElement("div");
@@ -11,13 +12,15 @@ let count = 0;
         y: data['bmpTemps'],
         type: 'line',
     }]);
+    count += data['bmpTemps'].length;
 
     setInterval(async () => {
         data = await fetch('/api/v0/packet/temperature/bmp?last=' + lastID);
         data = await data.json();
         Plotly.extendTraces('chart', { y: [data['bmpTemps']]}, [0]);
-        count++;
-        lastID = data['IDs'][data['IDs'].length - 1]
+        // count++;
+        lastID = data['IDs'][data['IDs'].length - 1];
+        count += data['bmpTemps'].length;
 
         if (count > 500) {
             Plotly.relayout('chart', {
